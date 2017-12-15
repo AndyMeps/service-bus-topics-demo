dea279b6d3d003e150b54d3f1638948d5fb1c416
Andy Mepham <andymeps@yahoo.com>
2017-12-15 16:10:55 +0000

    Code files added

--- ServiceBusTopicsDemo/Program.cs
+++ ServiceBusTopicsDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.ServiceBus;
+using System;
 
 namespace ServiceBusTopicsDemo
 {
@@ -6,7 +7,43 @@ namespace ServiceBusTopicsDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // SET VALUES HERE:
+            var endpoint = "sb://amtest.servicebus.windows.net/";
+            var topicName = "topic1";
+            var accessKeyName = "RootManageSharedAccessKey";
+            var accessKey = "jTnp29P9sQ5KMyvqCCRpMt5CkBalrLtRMYNhh5sik7Q=";
+            var subscription = "subscription1";
+            // DONE
+            
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("------------------");
+            Console.WriteLine("1. SendMessages");
+            Console.WriteLine("2. ReceiveMessages");
+            Console.WriteLine("------------------\n");
+
+            Console.Write("Enter choice: ");
+
+            var sbcs = GetConnectionStringBuilder(endpoint, topicName, accessKeyName, accessKey);
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    SendMessages.MainAsync(sbcs).GetAwaiter().GetResult();
+                    break;
+                case "2":
+                    ReceiveMessages.MainAsync(sbcs, subscription).GetAwaiter().GetResult();
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("Done, press ANY KEY to exit.");
+            Console.ReadKey();
+        }
+
+        static ServiceBusConnectionStringBuilder GetConnectionStringBuilder(string endpoint, string topicName, string accessKeyName, string accessKey, TransportType transportType = TransportType.AmqpWebSockets)
+        {
+            return new ServiceBusConnectionStringBuilder(endpoint, topicName, accessKeyName, accessKey, transportType);
         }
     }
 }
---
+++ ServiceBusTopicsDemo/SendMessages.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusTopicsDemo
+{
+    public static class SendMessages
+    {
+        static ITopicClient topicClient;
+
+        public static async Task MainAsync(ServiceBusConnectionStringBuilder sbcsb)
+        {
+            const int numberOfMessages = 10;
+            topicClient = new TopicClient(sbcsb);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the topic.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the topic.
+                    await topicClient.SendAsync(message);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception : {ex.Message}");
+            }
+        }
+    }
+}
